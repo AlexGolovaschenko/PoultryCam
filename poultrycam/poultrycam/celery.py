c307d882f8277eabe1860df8e3755c930a54989f
Aleksandr <golovasanya@mail.ru>
2021-07-23 14:17:52 +0300

    fix timezone for celery

--- poultrycam/poultrycam/celery.py
+++ poultrycam/poultrycam/celery.py
@@ -2,13 +2,12 @@ from __future__ import absolute_import, unicode_literals
 import os
 from datetime import timedelta
 from celery import Celery
-from django.conf import settings
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poultrycam.settings')
 
 app = Celery('poultrycam')
-app.conf.timezone = settings.TIME_ZONE
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
